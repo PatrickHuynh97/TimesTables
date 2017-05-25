f9e4d6bc4e34ff08466f3948f0106b0f2aa73dbc
k1502675 <k1502675@kcl.ac.uk>
2017-05-25 13:08:28 +0100

    Begun development of new game view, using more layouts for better scaling

---
+++ app/src/main/java/com/example/patrick/timestables/GameViewTest.java
@@ -0,0 +1,13 @@
+package com.example.patrick.timestables;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameViewTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_view_test);
+    }
+}
